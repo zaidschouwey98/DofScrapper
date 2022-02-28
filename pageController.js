f0e96bbc91257cb85b8aa30e4f4239a0ffe1c014
Schouwey <zaid.schouwey@gmail.com>
2022-02-28 09:34:16 +0100

    Finish scrapper

--- pageController.js
+++ pageController.js
@@ -1,10 +1,18 @@
 const pageScraper = require('./pageScraper');
+const fs = require('fs');
 async function scrapeAll(browserInstance){
 	let browser;
 	try{
 		browser = await browserInstance;
-		await pageScraper.scraper(browser);	
-		
+		let scrapedData = {};
+		scrapedData = await pageScraper.scraper(browser);	
+
+		fs.writeFile("data.json", JSON.stringify(scrapedData), 'utf8', function(err) {
+		    if(err) {
+		        return console.log(err);
+		    }
+		    console.log("The data has been scraped and saved successfully! View it at './data.json'");
+		});
 	}
 	catch(err){
 		console.log("Could not resolve the browser instance => ", err);
